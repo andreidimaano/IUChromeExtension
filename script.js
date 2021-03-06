4d19a9b432f4c6e72e6b36ef2b76a704756c87ae
Andrei Dimaano <andreiidimaano@gmail.com>
2021-03-06 14:52:00 -0800

    change to IU

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ function replaceText(element) {
         element.childNodes.forEach(replaceText);
         //text elements have no child nodes
     } else if (element.nodeType === Text.TEXT_NODE) {
-        if(element.textContent.match(/coronavirus/gi)) {
+        if(element.textContent.toLowerCase().includes('iu')) {
+            console.log('match', element.textContent);
             let newElement = document.createElement('span');
-            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, 
-            '<span class="rainbow">$1 </span>')
+            newElement.innerHTML = element.textContent.replace(/(iu)/gi, 
+            '<span class="rainbow">$1</span>')
             element.replaceWith(newElement)
             // element.parentElement.remove();
         }
